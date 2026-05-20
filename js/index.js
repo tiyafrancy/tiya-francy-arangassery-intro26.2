d58ffa8e72ff9b4072797bb161063149b420b634
Tiya Francy Arangassery <192169708+tiyafrancy@users.noreply.github.com>
2026-05-19 21:42:33 -0500

    adding background to nav session and adding footer

--- js/index.js
+++ js/index.js
@@ -2,6 +2,18 @@ const body = document.querySelector("body");
 
 const footer = document.createElement("footer");
 
-footer.textContent = "my webpage footer";
+// footer.textContent = "my webpage footer";
 
 body.appendChild(footer);
+
+const today = new Date();
+
+const thisYear = today.getFullYear();
+
+// const footer = document.querySelector("footer");
+
+const copyright = document.createElement("p");
+
+copyright.innerHTML = `&copy; Tiya Francy Arangassery ${thisYear}`;
+
+footer.appendChild(copyright);
