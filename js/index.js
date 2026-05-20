78673689e46242bf1c39baf2edbd4d7d271190c0
Tiya Francy Arangassery <192169708+tiyafrancy@users.noreply.github.com>
2026-05-20 10:20:40 -0500

    adding skillslist

--- js/index.js
+++ js/index.js
@@ -17,3 +17,17 @@ const copyright = document.createElement("p");
 copyright.innerHTML = `&copy; Tiya Francy Arangassery ${thisYear}`;
 
 footer.appendChild(copyright);
+
+const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
+
+const skillsSection = document.getElementById("skills");
+
+const skillsList = skillsSection.querySelector("ul");
+
+for (let i = 0; i < skills.length; i++){
+
+    const skill = document.createElement("li");
+    skill.innerHTML = skills[i];
+    skillsList.appendChild(skill);
+
+}
